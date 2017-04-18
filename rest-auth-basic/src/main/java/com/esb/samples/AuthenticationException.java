c381e596be434a339a181f024b6575f7250daafa
Janaka Bandara <janakaud@gmail.com>
2017-04-18 22:41:38 +0530

    sample on basic authentication for REST services

---
+++ rest-auth-basic/src/main/java/com/esb/samples/AuthenticationException.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2010-2017 AdroitLogic Private Ltd. (http://adroitlogic.org). All Rights Reserved.
+ *
+ * AdroitLogic PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.esb.samples;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
